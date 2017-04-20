edfd36fc9d13fd5a8f5902042ea42da610894aee
NadegdaMu <bykovanadegda@gmail.com>
2017-04-20 12:59:43 +0600

    Добавлен шаблонизатор для автарки пользователя и смены его на регистрацию

--- index.php
+++ index.php
@@ -28,7 +28,24 @@ $user_avatar = 'img/user.jpg';
 
         <nav class="user-menu">
 
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
+          <?php  $is_auth; ?>
+          <?php if ($is_auth == true): ?>
+            <div class="user-menu__image">
+              <img src="<?php $is_auth = $user_avatar ?>" width="40" height="40" alt="Пользователь">
+            </div>
+            <div class="user-menu__logged">
+              <p><?php $is_auth = $user_name ?></p>
+            </div>
+          <?php else: ?>
+            <ul class="user-menu__list">
+              <li class="user-menu__item">
+                <a href="#">Регистрация</a>
+              </li>
+              <li class="user-menu__item">
+                <a href="#">Вход</a>
+              </li>
+            </ul>
+          <?php endif; ?>
 
         </nav>
     </div>
