ae1f83a04203c99e40ba6fea0b4e3cecbb37a51f
NadegdaMu <bykovanadegda@gmail.com>
2017-04-23 19:54:47 +0600

    Вывод категорий и лотов из массивов

--- index.php
+++ index.php
@@ -14,8 +14,40 @@ $now = time();
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
 // ...
 
-
 $lot_time_remaining = date("H:i", mktime(0, 0, $tomorrow - $now));
+
+$categories_equipment = array("Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное");
+
+$announcement_list = array("announcement-1" => array('title'=>"2014 Rossignol District Snowboard",
+                                                   'category'=>"Доски и лыжи",
+                                                   'price'=>"10999",
+                                                   'URL-img'=>"/img/lot-1.jpg"),
+
+                           "announcement_2" => array('title'=>"DC Ply Mens 2016/2017 Snowboard",
+                                                   'category'=>"Доски и лыжи",
+                                                   'price'=>"159999",
+                                                   'URL-img'=>"/img/lot-2.jpg"),
+
+                           "announcement_3" => array('title'=>"Крепления Union Contact Pro 2015 года размер L/XL",
+                                                   'category'=>"Крепления",
+                                                   'price'=>"8000",
+                                                   'URL-img'=>"/img/lot-3.jpg"),
+
+                           "announcement_4" => array('title'=>"Ботинки для сноуборда DC Mutiny Charocal",
+                                                   'category'=>"Ботинки",
+                                                   'price'=>"10999",
+                                                   'URL-img'=>"/img/lot-4.jpg"),
+
+                           "announcement_5" => array('title'=>"Куртка для сноуборда DC Mutiny Charocal",
+                                                   'category'=>"Одежда",
+                                                   'price'=>"7500",
+                                                   'URL-img'=>"/img/lot-5.jpg"),
+
+                           "announcement_6" => array('title'=>"Маска Oakley Canopy",
+                                                   'category'=>"Разное",
+                                                   'price'=>"5400",
+                                                   'URL-img'=>"/img/lot-6.jpg"),
+)
 
 ?>
 <!DOCTYPE html>
@@ -81,21 +113,27 @@ $lot_time_remaining = date("H:i", mktime(0, 0, $tomorrow - $now));
         <div class="lots__header">
             <h2>Открытые лоты</h2>
             <select class="lots__select">
+
                 <option>Все категории</option>
+                <?php foreach ($categories_equipment as $value):?>
+                 <option><?=$value?></option>
+                <?php endforeach;?>
             </select>
+
         </div>
         <ul class="lots__list">
+            <?php foreach ($announcement_list as $key =>$value):?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$value['URL-img'] ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$value['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href=""><?=$value['title'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__amount">Начальная цена</span>
+                            <span class="lot__cost"><?=$value['price'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=$lot_time_remaining;?>
@@ -103,6 +141,7 @@ $lot_time_remaining = date("H:i", mktime(0, 0, $tomorrow - $now));
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
