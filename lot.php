fe3884099210d927084e8166e47f5ab69d275da2
NadegdaMu <bykovanadegda@gmail.com>
2017-04-24 10:05:47 +0600

    Добавлен отсчет времени лотов

--- lot.php
+++ lot.php
@@ -8,6 +8,8 @@
     ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
     ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
 ];*/
+
+
 $bets = array(
     array('name' => 'Иван', 'price' => 11500, 'ts' => strtotime('-' . rand(1, 50) .' minute')),
     array('name' => 'Константин', 'price' => 11000, 'ts' => strtotime('-' . rand(1, 18) .' hour')),
