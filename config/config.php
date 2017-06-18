8feda8c213851a0f558d74aabee9ca539ba1d578
Aleksandr Bykov <zlatdev@gmail.com>
2017-06-18 12:15:32 +0700

    new

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$config = [
+    
+    "db_host" => "localhost",
+
+    "db_port" => 3306,
+
+    "db_user" => "root",
+
+    "db_password" => "mysql",
+
+    "db_name" => "yeticave_db",
+
+
+];
+
+?> 
