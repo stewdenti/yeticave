0a17b38b650202c576b8d312f0f2107766f9de3e
zlatdev <zlatdev@gmail.com>
2017-06-19 13:37:54 +0300

    controllers

--- 200.php
+++ 200.php
@@ -1,5 +1,9 @@
 <?php
+require_once ("classes/Router.php");
 
+$c = new Router();
+
+var_dump ($c->parse());
 
 
 
---
+++ classes/controllers/Main.php
@@ -0,0 +1,9 @@
+<?php
+
+class Main extends BaseControllers 
+{
+
+    protected $template = "templates/main.php";
+    
+    public function 
+}
